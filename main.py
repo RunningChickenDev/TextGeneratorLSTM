8291923c9fb9079a7e7b01942ab807cffd64f042
MisterCavespider <cavespider12@gmail.com>
2018-02-05 19:55:53 +0100

    There's some magical python error

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from kivy.app import App
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock, mainthread
+from keras.callbacks import LambdaCallback, ModelCheckpoint
 import json
 import os
 import betternn as nn
@@ -61,7 +62,68 @@ class Modeller(Screen):
 			config.write(f)
 
 class Trainer(Screen):
-	pass
+	batchsize = StringProperty()
+	epochs = StringProperty()
+	weights = StringProperty()
+	prt = BooleanProperty()
+	fil = BooleanProperty()
+	wgt = BooleanProperty()
+
+	def onload(self):
+		config = cfg.ConfigParser()
+		config.sections()
+		config.read('cache/input.ini')
+
+		b = config['Trainer']['txt_batch_size']
+		print(type(b))
+
+		self.batchsize = "a string",
+		self.epochs = config['Trainer']['txt_epochs'],
+		self.weights = config['Trainer']['txt_weights'],
+		self.prt = bool(config['Trainer']['is_prt']),
+		self.fil = bool(config['Trainer']['is_fil']),
+		self.wgt = bool(config['Trainer']['is_wgt'])
+
+	def train(self):
+		# Try to store values
+		try:
+			nn.Data.vals['batch_size'] = int(self.ids['txt_batch_size'].text)
+			nn.Data.vals['epochs'] = int(self.ids['txt_epochs'].text)
+		except ValueError:
+			print("Could not parse a value!")
+
+		# Save config
+		if not os.path.exists("cache"):
+			os.makedirs("cache")
+		if not os.path.exists("cache/input.ini"):
+			f= open("cache/input.ini","w+")
+			f.close()
+		config = cfg.ConfigParser()
+		config.sections()
+		config.read('cache/input.ini')
+		config['Trainer'] = {
+			'txt_batch_size': self.ids['txt_batch_size'].text,
+			'txt_epochs': self.ids['txt_epochs'].text,
+			'txt_weights': self.ids['txt_weights'].text,
+			'is_prt': self.ids['is_prt'].active,
+			'is_fil': self.ids['is_fil'].active,
+			'is_wgt': self.ids['is_wgt'].active
+		}
+		with open("cache/input.ini", "w") as f:
+			config.write(f)
+
+		# Load callbacks
+		callbacks = []
+		if self.ids['is_prt'].active:
+			callbacks += [LambdaCallback(on_epoch_end=nn.prt_on_epoch_end)]
+		if self.ids['is_fil'].active:
+			callbacks += [LambdaCallback(on_epoch_end=nn.gen_on_epoch_end)]
+		if self.ids['is_wgt'].active:
+			out = self.ids['txt_weights'].text
+			callbacks += [ModelCheckpoint(out, monitor='loss', verbose=1, save_best_only=True, mode='min')]
+
+		# Train
+		nn.train(nn.Data.model, nn.Data.vals, callbacks)
 
 class Generator(Screen):
 	generated = StringProperty()
